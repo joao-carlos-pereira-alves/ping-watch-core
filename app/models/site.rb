6fae68b99b1bd4b52ef944be74a5fb49d5d63bf0
João Carlos <joaocarlopa@gmail.com>
2024-09-09 17:57:43 -0300

    feat: added edit and show site pages

--- app/models/site.rb
+++ app/models/site.rb
@@ -26,6 +26,57 @@ class Site < ApplicationRecord
     site_checks.average(:response_time_ms)&.to_f&.round(2)
   end
 
+  def site_checks_by_hour_of_day
+    data = site_checks
+           .group_by_hour_of_day(:created_at, format: '%H:%M')
+           .count
+
+    sorted_data = data.sort_by { |hour, _| hour }.to_h
+
+    {
+      labels: sorted_data.keys, # Horas do dia no formato "HH:MM"
+      datasets: [
+        {
+          label: I18n.t('chart.sites_checks_by_hour_of_day'),
+          backgroundColor: '#007bff', # Azul padrão
+          borderColor: '#007bff', # Azul padrão
+          data: sorted_data.values # Quantidade de checagens
+        }
+      ]
+    }
+  end
+
+  def site_status_distribution
+    # Contar a quantidade de cada status
+    status_counts = site_checks.group(:check_status).count
+  
+    # Definir cores para cada status
+    status_colors = {
+      'unknown' => '#6c757d',   # Cinza para Unknown
+      'online' => '#28a745',    # Verde para Online
+      'offline' => '#dc3545',   # Vermelho para Offline
+      'timeout' => '#ffc107',   # Amarelo para Timeout
+      'maintenance' => '#17a2b8', # Azul para Maintenance
+      'forbidden' => '#fd7e14'   # Laranja para Forbidden
+    }
+  
+    # Ordenar os dados por status
+    sorted_status_counts = status_counts.sort_by { |status, _| status }.to_h
+  
+    {
+      labels: sorted_status_counts.keys.map(&:capitalize), # Labels dos status
+      datasets: [
+        {
+          label: I18n.t('chart.site_status_distribution'), # Título do gráfico
+          backgroundColor: sorted_status_counts.keys.map { |status| status_colors[status] }, # Cores baseadas no status
+          borderColor: sorted_status_counts.keys.map { |status| status_colors[status] }, # Cores das bordas
+          data: sorted_status_counts.values # Quantidade de checagens para cada status
+        }
+      ]
+    }
+  end
+    
+
   def uptime_percentage
     total_checks = site_checks.count
     return 0 if total_checks.zero?
