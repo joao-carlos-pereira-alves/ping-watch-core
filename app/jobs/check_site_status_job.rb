1ae59c9ccff8c82d1ac6e4e2325df953782c1dc0
João Carlos <joaocarlopa@gmail.com>
2024-09-22 13:05:18 -0300

    feat: added notifications strategies

--- app/jobs/check_site_status_job.rb
+++ app/jobs/check_site_status_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CheckSiteStatusJob < ApplicationJob
   queue_as :default
 
--- app/jobs/check_sites_batch_job.rb
+++ app/jobs/check_sites_batch_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CheckSitesBatchJob < ApplicationJob
   queue_as :check_site_status
 
